ac7021d96a7097130a8e211e613881caf44df3e8
Shree Patel <36641237+RosePatels@users.noreply.github.com>
2025-08-14 20:55:35 -0400

    Add proxy to redirect requests

--- src/services/PotsService.ts
+++ src/services/PotsService.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 
 export async function getPots()  {
-    const result = axios.get('http://localhost:8080/api/pots');
+    const result = axios.get('../api/pots');
     return result;
 }
